325e27971b9afc30d067ba7ab782a25515d95d64
Luca A. Müller <luca.mueller@vestigas.com>
2022-06-28 13:46:08 +0200

    Adapt senece microservices

--- Assignment/product-descp-service/product_descp.js
+++ Assignment/product-descp-service/product_descp.js
@@ -8,5 +8,32 @@ module.exports = function (options) {
 
 
     //To DO: add the pattern functions and describe the logic inside the function
+    function productURL(msg, respond) {
+        if(msg.productId){
+            console.log(`getProductURL -> id ${msg.productId}`)
+            var res = mockData.filter((product) => { return product["product_id"] == msg.productId})
+            if (res.length > 0) {
+                respond(null, { result: res[0].product_url });
+            } else
+                respond(null, { result: ''});
+        }
+        else {
+            respond(null, { result: ''});
+        }
+    }
 
+    function productName(msg, respond) {
+        if(msg.productId){
+            console.log(`getProductName -> id ${msg.productId}`)
+            var res = mockData.filter((product) => { return product["product_id"] == msg.productId})
+            if (res.length > 0) {
+                respond(null, { result: res[0].product_name });
+            } else {
+                respond(null, { result: ''});
+            }
+        }
+        else {
+            respond(null, { result: ''});
+        }
+    }
 }
--- Assignment/product-price-service/product_price.js
+++ Assignment/product-price-service/product_price.js
@@ -1,9 +1,22 @@
 module.exports = function (options) {
     //Import the mock data json file
     const mockData = require('./MOCK_DATA.json');
-    //To DO: Add the patterns and their corresponding functions
+    this.add('role:product,cmd:getProductPrice', productPrice);
 
 
     //To DO: add the pattern functions and describe the logic inside the function
+    function productPrice(msg, respond) {
+        if(msg.productId){
+            console.log(`getProductPrice -> id ${msg.productId}`)
+            var res = mockData.filter((product) => { return product["product_id"] == msg.productId})
+            if (res.length > 0) {
+                respond(null, { result: res[0].product_price });
+            } else
+                respond(null, { result: ''});
+        }
+        else {
+            respond(null, { result: ''});
+        }
+    }
 
 }
